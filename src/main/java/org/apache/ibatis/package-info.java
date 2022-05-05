70b340ff8c0739db4aac79a051476d0644d4e613
yzy <yezy@daishupei.com>
2022-05-05 09:38:31 +0800

    git

--- src/main/java/org/apache/ibatis/package-info.java
+++ src/main/java/org/apache/ibatis/package-info.java
@@ -14,6 +14,6 @@
  *    limitations under the License.
  */
 /**
- * The MyBatis data mapper framework makes it easier to use a relational database with object-oriented applications.
+ * The MyBatis data mapper framework makes it easier to use a relational database with object-oriented applications. git
  */
 package org.apache.ibatis;
